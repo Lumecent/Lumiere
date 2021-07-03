9039dea2c7a4b1b9e887ebfc788afaac6b1cfb58
Stanislav Ermolaev <lumius.dev@gmail.com>
2021-07-04 01:10:02 +0300

    Added command generate middleware

---
+++ app/Base/Generate/Commands/GenerateMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Base\Generate\Commands;
+
+use App\Base\Generate\GenerateCommand;
+
+class GenerateMiddleware extends GenerateCommand
+{
+    protected $signature = 'lumiere:middleware {middleware}';
+
+    protected $description = 'Create a new middleware';
+
+    public function handle(): void
+    {
+        $this->createFile( 'middleware', 'App\Base\Middleware', 'middleware' );
+        $this->info( 'Middleware created!' );
+    }
+}
