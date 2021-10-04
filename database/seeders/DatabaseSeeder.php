aee224503db63d8fe01c61a555a70bae6ef45c91
Stanislav Ermolaev <lumius.dev@gmail.com>
2021-10-04 21:54:44 +0300

    Update DatabaseSeeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Abstractions\Seeders\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call( [
+
+        ] );
+    }
+}
