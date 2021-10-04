04428356e1cb44a064c1b193aad708144f3c9ecc
Stanislav Ermolaev <lumius.dev@gmail.com>
2021-10-04 21:57:01 +0300

    Factory abstraction added

---
+++ app/Abstractions/Factories/Factory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Abstractions\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory as IlluminateFactory;
+
+abstract class Factory extends IlluminateFactory
+{
+
+}
