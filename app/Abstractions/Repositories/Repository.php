0b9b981fca51891aa78d63a552879652f0d0c731
Stanislav Ermolaev <lumius.dev@gmail.com>
2021-11-28 22:32:03 +0300

    Repository fix

--- app/Abstractions/Repositories/Repository.php
+++ app/Abstractions/Repositories/Repository.php
@@ -6,15 +6,10 @@ use App\Abstractions\Models\Model;
 
 abstract class Repository
 {
-    private static ?Model $model = null;
-
     public static function startConditions(): Model
     {
-        if ( is_null( self::$model ) ) {
-            $model = static::getModelClass();
-            self::$model = new $model();
-        }
-        return self::$model;
+        $model = static::getModelClass();
+        return new $model();
     }
 
     abstract protected static function getModelClass(): string;
