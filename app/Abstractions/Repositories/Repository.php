e97c7c46798784ecc33be956183e1967dc0272a9
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-03-29 21:39:16 +0300

    Repository added some methods

--- app/Abstractions/Repositories/Repository.php
+++ app/Abstractions/Repositories/Repository.php
@@ -2,18 +2,25 @@
 
 namespace App\Abstractions\Repositories;
 
-use App\Abstractions\Entity\Entity;
+use App\Abstractions\DTO\Dto;
+use App\Abstractions\Entities\Entity;
 use App\Abstractions\Models\Model;
 
 abstract class Repository
 {
+    abstract protected static function getModelClass(): string;
+
+    abstract protected function getEntity( ?Model $model ): ?Entity;
+
+    abstract public function create( Dto $dto ): ?Entity;
+
+    abstract public function update( Dto $dto ): ?Entity;
+
+    abstract public function delete( Dto $dto ): bool;
+
     public function startConditions(): Model
     {
         $model = static::getModelClass();
         return new $model();
     }
-
-    abstract protected static function getModelClass(): string;
-
-    abstract protected static function getEntity( Model $model ): Entity;
 }
