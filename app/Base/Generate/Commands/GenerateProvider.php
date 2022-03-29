42ac0ccec0d58ea27414299939a85817a3dab07c
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-03-29 21:44:12 +0300

    Added command generate provider

---
+++ app/Base/Generate/Commands/GenerateProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Base\Generate\Commands;
+
+use App\Base\Generate\GenerateCommand;
+use App\Utilities\Helpers\FilesystemHelper;
+use Illuminate\Support\Facades\Artisan;
+
+class GenerateProvider extends GenerateCommand
+{
+    protected $signature = 'lumiere:provider {provider} {mode=interactive} {container?} {--m}';
+
+    protected $description = 'Create a new providers';
+
+    protected function processGenerateFile( $params ): void
+    {
+        [ $container ] = $params;
+
+        FilesystemHelper::createDir( "app/Containers/$container/Providers" );
+
+        $this->createFile( [ 'provider', "App\Containers\\$container\Providers" ], 'provider', 'Provider' );
+        $this->info( 'Provider created!' );
+    }
+}
